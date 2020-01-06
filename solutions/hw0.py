25d39c0d7fe021c56426c0b3c25c7604356106db
Anna Iashina <95schatt@gmail.com>
2020-01-06 20:55:30 +0300

    1-4 questions

--- solutions/hw0.py
+++ solutions/hw0.py
@@ -4,6 +4,7 @@ def main():
     test()
 
     import snap
+    import numpy as np
 
     g = snap.LoadEdgeList(snap.PNGraph, '/Users/anna/Documents/repositories/cs224w/data/wiki-Vote.txt', 0, 1)
 
@@ -12,9 +13,16 @@ def main():
 
     loops = set()
     for e in g.Edges():
-        if e.GetSrcNId == e.GetDstNId:
-            loops.add(e.GetSrcNId)
-    print("Number of nodes with loops is", len(loops))
+        if e.GetSrcNId() == e.GetDstNId():
+            loops.add(e.GetSrcNId())
+    n = snap.CntSelfEdges(g)
+    print("Number of nodes with loops is", n)
+
+    n = g.GetEdges()
+    print("Number of directed edges is", n)
+
+    reciprocal = [g.IsEdge(E.GetDstNId(), E.GetSrcNId()) for E in g.Edges()]
+    print("Number of reciprocal edges is", np.sum(reciprocal))
 
 
 if __name__ == "__main__":
